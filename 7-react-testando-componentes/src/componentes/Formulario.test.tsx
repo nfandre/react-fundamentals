c09e8df6be3b920f865c0500cd9e99302d042dbc
nfandre <andrelive.05@hotmail.com>
2025-12-19 11:59:24 -0300

    test recoil estado

--- 7-react-testando-componentes/src/componentes/Formulario.test.tsx
+++ 7-react-testando-componentes/src/componentes/Formulario.test.tsx
@@ -1,5 +1,4 @@
 import { act, fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
 import { RecoilRoot } from "recoil";
 import Formulario from "./Formulario";
 
@@ -19,34 +18,34 @@ describe('o comportamento do Formulario.tsx', () => {
         // garantir que o botão esteja desabilitado
         expect(botao).toBeDisabled()
     })
-
+    
     test('adicionar um participante caso exista um nome preenchido', () => {
         render(
             <RecoilRoot>
                 <Formulario />
             </RecoilRoot>)
-
+    
         // encontrar no DOM o input
         const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
         // encontrar o botão
         const botao = screen.getByRole('button')
-
+    
         // inserir um valor no input
         fireEvent.change(input, {
             target: {
                 value: 'Ana Catarina'
             }
         })
-
+    
         // clicar no botão de submeter
         fireEvent.click(botao)
-
+    
         // garantir que o input esteja com o foco ativo
         expect(input).toHaveFocus()
         // garantir que o input não tenha um valor
         expect(input).toHaveValue("")
     })
-
+    
     test('nomes duplicados não podem ser adicionados na lista', () => {
         render(
             <RecoilRoot>
@@ -66,12 +65,12 @@ describe('o comportamento do Formulario.tsx', () => {
             }
         })
         fireEvent.click(botao)
-
+    
         const mensagemDeErro = screen.getByRole('alert')
-
+    
         expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos!')
     })
-
+    
     test('a mensagem de erro deve sumir após os timers', () => {
         jest.useFakeTimers()
         render(
@@ -94,12 +93,12 @@ describe('o comportamento do Formulario.tsx', () => {
         fireEvent.click(botao)
         let mensagemDeErro = screen.queryByRole('alert')
         expect(mensagemDeErro).toBeInTheDocument()
-
+    
         act(() => {
             jest.runAllTimers()
         });
-
+    
         mensagemDeErro = screen.queryByRole('alert')
         expect(mensagemDeErro).toBeNull()
     })
-});
+})
---
+++ 7-react-testando-componentes/src/componentes/Rodape.test.tsx
@@ -0,0 +1,72 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { RecoilRoot } from "recoil"
+import Rodape from "./Rodape";
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+
+jest.mock('../state/hook/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+});
+
+const mockNavegacao = jest.fn()
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavegacao
+    };
+});
+
+describe('quando não existem participantes suficientes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+    });
+    test('a brincadeira não pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+
+        expect(botao).toBeDisabled();
+    });
+})
+
+describe('quando existem participantes suficientes', () => {
+
+    const participantes = ['ana', 'catarina', 'josefina'];
+
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+    });
+
+    test('a brincadeira pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+
+        expect(botao).toBeEnabled();
+    });
+
+    test('a brincadeira foi iniciada', () => {
+
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+
+        fireEvent.click(botao);
+
+        expect(mockNavegacao).toHaveBeenCalledTimes(1);
+        expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
+    });
+})
